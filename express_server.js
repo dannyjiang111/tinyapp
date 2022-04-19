1e88574e181caa3812a7e73e773619243cf67c63
Danny Jiang <dannyjiang11@outlook.com>
2022-04-19 12:26:18 +0000

    added route to urls.json

--- express_server.js
+++ express_server.js
@@ -1,16 +1,29 @@
+// DEPENDENCIES //
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+// VARIABLES //
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+// GET REQUESTS //
+// Prints out Hello when accessing the root path / //
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+// Prints out object urlDatabase //
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+// POST REQUESTS //
+
+
+// LISTENS TO PORT //
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
