df5550569a04484d96c1f865fbc29304682e8acf
Danny Jiang <dannyjiang11@outlook.com>
2022-04-22 04:33:49 +0000

    added a POST to redirect new url to short url database

--- express_server.js
+++ express_server.js
@@ -1,3 +1,13 @@
+const generateRandomString = () => {
+  let randomString = "";
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const stringLength = 6;
+  for (let i = 0; i < stringLength; i++) {
+    randomString += characters[Math.floor(Math.random() * characters.length)];
+  }
+  return randomString;
+};
+
 // DEPENDENCIES //
 const express = require("express");
 const app = express();
@@ -5,6 +15,8 @@ const PORT = 8080; // default port 8080
 
 // MIDDLEWARE //
 app.set("view engine", "ejs"); // Tells Express app to use EJS as a templating engine 
+const bodyParser = require("body-parser"); // Helps make data readable
+app.use(bodyParser.urlencoded({extended: true}));
 
 // VARIABLES //
 const urlDatabase = {
@@ -13,35 +25,46 @@ const urlDatabase = {
 };
 
 // GET REQUESTS //
-// Prints out Hello when accessing the root path / //
+// Prints out Hello when accessing the root path / 
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-// Prints out object urlDatabase //
+// Prints out object urlDatabase 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-// Prints out Hello World //
+// Prints out Hello World 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-// Renders main page //
+// Renders main page 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
-// Directs to modified shortURL page with edit //
+// Renders the page with the form 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+// Directs to modified shortURL page with edit 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
 });
 
+
 // POST REQUESTS //
 
+// Adds new url to database and redirects to short url page //
+app.post("/urls", (req, res) => {
+  console.log(req.body);  
+  res.send("Ok");         
+});
 
 // LISTENS TO PORT //
 app.listen(PORT, () => {
